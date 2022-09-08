b1792a5f00ef1bce12be714fb458c32c0d0e1484
Michael Portelas <michael.portelas@gmail.com>
2022-09-08 19:35:09 +0200

    P2C3

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
 import { SurveyContext } from '../../utils/context'
-import { useFetch } from '../../utils/hooks'
+import { useFetch, useTheme } from '../../utils/hooks'
 
 const SurveyContainer = styled.div`
   display: flex;
@@ -16,16 +16,18 @@ const SurveyContainer = styled.div`
 const QuestionTitle = styled.h2`
   text-decoration: underline;
   text-decoration-color: ${colors.primary};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const QuestionContent = styled.span`
   margin: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const LinkWrapper = styled.div`
   padding-top: 30px;
   & a {
-    color: black;
+    color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
   }
   & a:first-of-type {
     margin-right: 20px;
@@ -39,7 +41,9 @@ const ReplyBox = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${colors.backgroundLight};
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
   border-radius: 30px;
   cursor: pointer;
   box-shadow: ${(props) =>
@@ -62,10 +66,12 @@ function Survey() {
   const questionNumberInt = parseInt(questionNumber)
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
   const nextQuestionNumber = questionNumberInt + 1
+
+  const { theme } = useTheme()
   const { saveAnswers, answers } = useContext(SurveyContext)
 
   const { data, isLoading, error } = useFetch('http://localhost:8000/survey')
-  const { surveyData } = data
+  const surveyData = data?.surveyData
   
   function saveReply(answer) {
     saveAnswers({ [questionNumber]: answer })
@@ -78,11 +84,11 @@ function Survey() {
   
   return (
     <SurveyContainer>
-      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      <QuestionTitle theme={theme}>Question {questionNumber}</QuestionTitle>
       {isLoading ? (
         <Loader />
       ) : (
-        <QuestionContent>
+        <QuestionContent theme={theme}>
             {surveyData && surveyData[questionNumber]}
         </QuestionContent>
       )}
@@ -91,20 +97,22 @@ function Survey() {
           <ReplyBox
             onClick={() => saveReply(true)}
             isSelected={answers[questionNumber] === true}
+            theme={theme}
           >
             Oui
           </ReplyBox>
           <ReplyBox
             onClick={() => saveReply(false)}
             isSelected={answers[questionNumber] === false}
+            theme={theme}
           >
             Non
           </ReplyBox>
         </ReplyWrapper>
       )}
-      <LinkWrapper>
+      <LinkWrapper theme={theme}>
         <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-        {data[questionNumberInt + 1] ? (
+        {surveyData[questionNumberInt + 1] ? (
           <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
         ) : (
           <Link to="/results">Résultats</Link>
