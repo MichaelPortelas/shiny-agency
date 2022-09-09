988da5ed44fd52d192184ae7b2405d09bd80bedc
Michael Portelas <michael.portelas@gmail.com>
2022-09-09 10:16:47 +0200

    P3C2 solution

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,65 +1,75 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
+import { useTheme } from '../../utils/hooks'
 import DefaultPicture from '../../assets/profile.png'
 
 const CardLabel = styled.span`
-    color: ${colors.primary};
-    font-size: 22px;
-    font-weight: normal;
-    padding-left: 15px;
+  color: ${({ theme }) => (theme === 'light' ? colors.primary : '#ffffff')};
+  font-size: 22px;
+  font-weight: normal;
+  padding-left: 15px;
 `
 
-const CardTitle = styled.span`
-    color: black;
-    font-size: 22px;
-    font-weight: normal;
-    align-self: center;
+const CardTitle = styled.div`
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
+  height: 25px;
+  display: flex;
+  align-items: center;
 `
 
 const CardImage = styled.img`
-    height: 150px;
-    width: 150px;
-    align-self: center;
-    border-radius: 50%;
+  height: 150px;
+  width: 150px;
+  align-self: center;
+  border-radius: 50%;
 `
 
 const CardWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    padding: 15px;
-    background-color: ${colors.backgroundLight};
-    border-radius: 30px;
-    width: 300px;
-    height: 300px;
-    transition: 200ms;
-    &:hover {
-        cursor: pointer;
-        box-shadow: 2px 2px 10px #e2e3e9;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 15px;
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+  border-radius: 30px;
+  width: 300px;
+  height: 300px;
+  &:hover {
+    cursor: pointer;
+  }
 `
 
 function Card({ label, title, picture }) {
-    return (
-        <CardWrapper>
-            <CardLabel>{label}</CardLabel>
-            <CardImage src={picture} alt="freelance" />
-            <CardTitle>{title}</CardTitle>
-        </CardWrapper>
-    )
+  const { theme } = useTheme()
+  const [isFavorite, setIsFavorite] = useState(false)
+  const star = isFavorite ? '⭐️' : ''
+
+  return (
+    <CardWrapper theme={theme} onClick={() => setIsFavorite(!isFavorite)}>
+      <CardLabel theme={theme}>{label}</CardLabel>
+      <CardImage src={picture} alt="freelance" />
+      <CardTitle theme={theme}>
+        {star} {title} {star}
+      </CardTitle>
+    </CardWrapper>
+  )
 }
 
 Card.propTypes = {
-    label: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
 }
 
 Card.defaultProps = {
-    label: '',
-    title: '',    
-    picture: DefaultPicture,
+  label: '',
+  title: '',
+  picture: DefaultPicture,
 }
- 
+
 export default Card
---
+++ src/components/Card/index.test.js
@@ -0,0 +1,45 @@
+import Card from './'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { ThemeProvider } from '../../utils/context'
+
+describe('Card', () => {
+    test('should render without crash', async () => {
+        render(
+            <ThemeProvider>
+                <Card />
+            </ThemeProvider>
+        )
+    })
+
+    test('Should render title and image', async () => {
+        render(
+            <ThemeProvider>
+                <Card 
+                    title="Harry Potter"
+                    label="Magicien frontend"
+                    picture="/myPicture.png"
+                />
+            </ThemeProvider>
+        )
+        const cardPicture = screen.getByRole('img')
+        const cardTitle = screen.getByText(/Harry/i)
+        expect(cardPicture.src).toBe('http://localhost/myPicture.png')
+        expect(cardTitle.textContent).toBe(' Harry Potter ')
+    })
+
+    test('Should add ⭐️ around title', async () => {
+        render(
+          <ThemeProvider>
+            <Card
+              title="Harry Potter"
+              label="Magicien frontend"
+              picture="/myPicture.png"
+            />
+          </ThemeProvider>
+        )
+        const cardTitle = screen.getByText(/Harry/i)
+        const parentNode = cardTitle.closest('div')
+        fireEvent.click(parentNode)
+        expect(cardTitle.textContent).toBe('⭐️ Harry Potter ⭐️')
+    })
+})
